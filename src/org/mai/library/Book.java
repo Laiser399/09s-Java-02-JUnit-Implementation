b2c22ea624016069194c1b32f241ec1623b6796c
Laiser399 <Laiser399@mail.ru>
2021-09-19 21:07:10 +0300

    immutable comparable Book

--- src/org/mai/library/Book.java
+++ src/org/mai/library/Book.java
@@ -1,6 +1,32 @@
 package org.mai.library;
 
+import java.util.Objects;
+
 public class Book {
-    int id;
-    String title;
+    public final int id;
+    public final String title;
+
+    public Book(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Book book = (Book) o;
+        return id == book.id
+                && Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title);
+    }
 }
