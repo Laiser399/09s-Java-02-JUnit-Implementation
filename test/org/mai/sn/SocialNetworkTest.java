f7756a9eb0572af65507afd3f39dc7557ea37ecb
Laiser399 <Laiser399@mail.ru>
2021-09-19 18:50:57 +0300

    fails для SocialNetworkTest

--- test/org/mai/sn/SocialNetworkTest.java
+++ test/org/mai/sn/SocialNetworkTest.java
@@ -1,6 +1,7 @@
 package org.mai.sn;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,21 +9,26 @@ class SocialNetworkTest {
 
     @BeforeEach
     void setUp() {
+        Assertions.fail();
     }
 
     @AfterEach
     void tearDown() {
+        Assertions.fail();
     }
 
     @Test
     void addPerson() {
+        Assertions.fail();
     }
 
     @Test
     void addConnection() {
+        Assertions.fail();
     }
 
     @Test
     void getFriends() {
+        Assertions.fail();
     }
 }
